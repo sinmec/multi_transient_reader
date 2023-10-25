b48f6f63acc5f34b351ffeb6973bcf121d7707c6
Rafael Cerqueira <rafael.f.l.cerqueira@gmail.com>
2023-10-24 22:05:14 -0300

    Reformat code and improve descriptions

--- src/statistical_tools/common_functions.py
+++ src/statistical_tools/common_functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def compute_temporal_average(time_values, transient_values):
     avg_value = np.trapz(transient_values, x=time_values)
-    avg_value /= (time_values.max() - time_values.min())
+    avg_value /= time_values.max() - time_values.min()
 
     return avg_value
 
@@ -12,7 +12,7 @@ def compute_temporal_std(time_values, transient_values):
     avg_value = compute_temporal_average(time_values, transient_values)
 
     std_value = np.trapz(np.power(transient_values - avg_value, 2.0), x=time_values)
-    std_value /= (time_values.max() - time_values.min())
+    std_value /= time_values.max() - time_values.min()
     std_value = np.sqrt(std_value)
 
     return std_value
--- tests/test_common_functions.py
+++ tests/test_common_functions.py
@@ -1,7 +1,17 @@
 import numpy as np
 import pytest
 
-from src.statistical_tools.common_functions import compute_temporal_average, compute_temporal_std
+from src.statistical_tools.common_functions import (
+    compute_temporal_average,
+    compute_temporal_std,
+)
+
+MIN_POINTS = 5000
+MAX_POINTS = 100000
+N_TEST_POINTS = 10
+MIN_Y_VALUE = 10.0
+MAX_Y_VALUE = 100000.0
+N_Y_VALUES = 10
 
 
 def uniform_sine_wave(sine_parameters):
@@ -16,15 +26,23 @@ def uniform_sine_wave(sine_parameters):
 def non_uniform_sine_wave(sine_parameters):
     t_start = 0.0
     t_end = 2.0 * np.pi
-    time_values = np.random.uniform(low=t_start, high=t_end, size=sine_parameters["total_points"])
+    time_values = np.random.uniform(
+        low=t_start, high=t_end, size=sine_parameters["total_points"]
+    )
     time_values.sort()
-    transient_values = sine_parameters['y_value'] + np.sin(time_values)
+    transient_values = sine_parameters["y_value"] + np.sin(time_values)
 
     return time_values, transient_values
 
 
-@pytest.mark.parametrize("total_points", np.linspace(10000, 100000, num=4, endpoint=True, dtype=int))
-@pytest.mark.parametrize("y_value", np.linspace(10.0, 100000, num=10, endpoint=True, dtype=float))
+@pytest.mark.parametrize(
+    "total_points",
+    np.linspace(MIN_POINTS, MAX_POINTS, num=N_TEST_POINTS, endpoint=True, dtype=int),
+)
+@pytest.mark.parametrize(
+    "y_value",
+    np.linspace(MIN_Y_VALUE, MAX_Y_VALUE, num=N_Y_VALUES, endpoint=True, dtype=float),
+)
 @pytest.mark.parametrize("test_function", [uniform_sine_wave, non_uniform_sine_wave])
 def test_compute_temporal_average(total_points, y_value, test_function):
     """
@@ -39,17 +57,23 @@ def test_compute_temporal_average(total_points, y_value, test_function):
     assert average_value == pytest.approx(y_value, rel=1.0e-3)
 
 
-@pytest.mark.parametrize("total_points", np.linspace(10000, 100000, num=4, endpoint=True, dtype=int))
-@pytest.mark.parametrize("y_value", np.linspace(10.0, 100000, num=10, endpoint=True, dtype=float))
+@pytest.mark.parametrize(
+    "total_points",
+    np.linspace(MIN_POINTS, MAX_POINTS, num=N_TEST_POINTS, endpoint=True, dtype=int),
+)
+@pytest.mark.parametrize(
+    "y_value",
+    np.linspace(MIN_Y_VALUE, MAX_Y_VALUE, num=N_Y_VALUES, endpoint=True, dtype=float),
+)
 @pytest.mark.parametrize("test_function", [uniform_sine_wave, non_uniform_sine_wave])
 def test_compute_temporal_std(total_points, y_value, test_function):
     """
     This test checks if the temporal standard deviation values are being computed correctly.
     This is important in cases where the transient values have a variable timestep.
+    For a sine wave on the interval [0, 2 pi], the std value should return sqrt(0.5).
     """
 
-    sine_parameters = {'total_points': total_points,
-                       'y_value': y_value}
+    sine_parameters = {"total_points": total_points, "y_value": y_value}
     time_values, transient_values = test_function(sine_parameters)
     average_value = compute_temporal_std(time_values, transient_values)
 
